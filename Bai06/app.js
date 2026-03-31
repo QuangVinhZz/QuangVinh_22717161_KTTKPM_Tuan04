bb7627615f8f010b7dd32af5bd138835641610ad
Le Nguyen Quang Vinh <quangvinh7137@gmail.com>
2026-03-31 12:04:16 +0700

    complete test docker

---
+++ Bai06/app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello Multi-stage Docker!");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ Bai07/app.py
@@ -0,0 +1,5 @@
+import os
+
+app_env = os.getenv("APP_ENV")
+
+print(f"APP_ENV: {app_env}")
